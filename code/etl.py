413ccc30e5ecfe7285a25dd383ae57bd4fc312d0
Josie Smith <jsmit194@syr.edu>
2025-05-03 21:10:44 -0400

    etl.py functions complete

--- code/etl.py
+++ code/etl.py
@@ -3,18 +3,55 @@ import streamlit as st
 
 
 def top_locations(violations_df : pd.DataFrame, threshold=1000) -> pd.DataFrame:
-    return pd.DataFrame()  # TODO implement this function
+    '''Return a dataframe of the locations of parking tickets with $1,000 or more in total aggregate violation amounts.  
+This dataframe is keyed on location (1 row per location) and has two columns: location and amount.  
+There should be 135 rows in this dataframe.'''
+    amount_by_location_df = violations_df.pivot_table(index='location', values='amount', aggfunc='sum').sort_values(by='amount', ascending=False)
+    amount_by_location_df['location'] = amount_by_location_df.index
+    amount_by_location_df.reset_index(drop=True, inplace=True)
+    amount_by_location_df = amount_by_location_df[amount_by_location_df['amount'] >= threshold]
+    amount_by_location_df = amount_by_location_df[['location', 'amount']]
+    return amount_by_location_df.reset_index(drop=True)
 
 
 def top_locations_mappable(violations_df : pd.DataFrame, threshold=1000) -> pd.DataFrame:
-    return pd.DataFrame()  # TODO implement this function
-
+    '''
+get top locations then add lat and long from the original dataframe.  
+Again this dataframe is keyed on location (1 row per location) and 4 columns: location, lat, lon, amount  
+Make sure you have the same number of rows as the top_locations dataframe
+    '''
+    top_locations_df = top_locations(violations_df, threshold)
+    violations_pre_merge_df = violations_df[['location', 'lat', 'lon']].drop_duplicates(subset=['location'])
+    merged_df = pd.merge(top_locations_df, violations_pre_merge_df, on='location')
+    return merged_df.drop_duplicates().reset_index(drop=True)[['location', 'lat', 'lon', 'amount']]
 
 def tickets_in_top_locations(violations_df : pd.DataFrame, threshold=1000) -> pd.DataFrame:
-    return pd.DataFrame()  # TODO implement this function
+    '''
+Return a dataframe of the parking tickets that were issued in the top locations.  
+This dataframe is keyed on ticket number and has the same columns as the original dataframe.  
+It should just be a subset of the original dataframe where the location one of the top locations.  
+There should be 8,109 rows in this dataframe.
+    '''
+    top_locations_df = top_locations(violations_df)
+    merged_df = pd.merge(top_locations_df[['location']], violations_df, on='location')
+    return merged_df
 
 if __name__ == '__main__':
     '''
-    Main ETL job. 
+    should read in the input dataframe, call your functions, and write the output dataframes to CSV files.
     '''
-    pass # TODO implement this script
+    print("Running ETL job...")
+    print("Reading violations data from ./cache/final_cuse_parking_violations.csv")
+    violations_df = pd.read_csv('./cache/final_cuse_parking_violations.csv')
+    
+    top_locations_df = top_locations(violations_df)
+    print("Writing top locations to ./cache/top_locations.csv")
+    top_locations_df.to_csv('./cache/top_locations.csv', index=False)
+    
+    top_locations_mappable_df = top_locations_mappable(violations_df)
+    print("Writing mappable top locations to ./cache/top_locations_mappable.csv")
+    top_locations_mappable_df.to_csv('./cache/top_locations_mappable.csv', index=False)
+
+    tickets_in_top_locations_df = tickets_in_top_locations(violations_df, top_locations_df)
+    print("Writing tickets in top locations to ./cache/tickets_in_top_locations.csv")
+    tickets_in_top_locations_df.to_csv('./cache/tickets_in_top_locations.csv', index=False)
--- tests/test_etl.py
+++ tests/test_etl.py
@@ -1,5 +1,13 @@
-import pandas as pd
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'code'))) # had to add this line to get the import to work
+print(f"Current sys.path: {sys.path}")  # Check if 'code' folder is in sys.path
+
 from code.etl import top_locations, top_locations_mappable, tickets_in_top_locations
+
+import pandas as pd
+
+
 
 def test_should_pass():
     print("\nAlways True!")
@@ -16,13 +24,13 @@ def test_top_locations():
     top_locations_df = top_locations(violations_df)
 
     # assert what is expected actually happened
-    print("\nTesting row count of {expect_row_count}...")
+    print(f"\nTesting row count of {expect_row_count}...")
     assert len(top_locations_df) == expect_row_count
     
-    print("\nTesting column count of {expect_col_count}...")
+    print(f"\nTesting column count of {expect_col_count}...")
     assert len(top_locations_df.columns) == expect_col_count
 
-    print("\nTesting column names are  {expect_col_names}...")
+    print(f"\nTesting column names are  {expect_col_names}...")
     assert set(top_locations_df.columns) == set(expect_col_names)
 
 def test_top_locations_mappable():
@@ -36,13 +44,13 @@ def test_top_locations_mappable():
     top_locations_df = top_locations_mappable(violations_df)
 
     # assert what is expected actually happened
-    print("\nTesting row count of {expect_row_count}...")
+    print(f"\nTesting row count of {expect_row_count}...")
     assert len(top_locations_df) == expect_row_count
     
-    print("\nTesting column count of {expect_col_count}...")
+    print(f"\nTesting column count of {expect_col_count}...")
     assert len(top_locations_df.columns) == expect_col_count
 
-    print("\nTesting column names are  {expect_col_names}...")
+    print(f"\nTesting column names are  {expect_col_names}...")
     assert set(top_locations_df.columns) == set(expect_col_names)
 
 def test_tickets_in_top_locations():
@@ -58,11 +66,11 @@ def test_tickets_in_top_locations():
     top_locations_df =tickets_in_top_locations(violations_df)
 
     # assert what is expected actually happened
-    print("\nTesting row count of {expect_row_count}...")
+    print(f"\nTesting row count of {expect_row_count}...")
     assert len(top_locations_df) == expect_row_count
     
-    print("\nTesting column count of {expect_col_count}...")
+    print(f"\nTesting column count of {expect_col_count}...")
     assert len(top_locations_df.columns) == expect_col_count
 
-    print("\nTesting column names are  {expect_col_names}...")
+    print(f"\nTesting column names are  {expect_col_names}...")
     assert set(top_locations_df.columns) == set(expect_col_names)
